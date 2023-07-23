91181a70a9de07f3c044b21ee39fb7d876f7424b
Hyun-Tae Jin <htjmario@gmail.com>
2023-07-22 23:33:08 -0400

    added avatar, experimenting with template

---
+++ src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export function Logo() {
+  return (
+    <Image
+      src={require('../images/avatar.png')}
+      width={200}
+      height={200}
+      alt="Avatar"
+    />
+  )
+}
